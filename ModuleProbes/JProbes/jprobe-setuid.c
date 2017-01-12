5b43d313abbe0a3e52dfa36bef90e5b7602479e7
Sp4rt4h <Sp4rt4h@gmail.com>
2017-01-11 21:19:57 -0600

    Add files via upload

--- ModuleProbes/JProbes/jprobe-setuid.c
+++ ModuleProbes/JProbes/jprobe-setuid.c
@@ -1,1 +1,40 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/kprobes.h>
+#include <linux/kallsyms.h>
+static struct jprobe setuid_jprobe;
+static asmlinkage int
+{
+	printk("process %s [%d] attempted setuid to %d/n",current->comm,current->cred->uid, uid);
+	jprobe_return();
+	return 0;
+}
+int init_module(void)
+{
+	int ret;
+	setuid_jprobe.entry = (kprobe_opcode_t *) kp_setuid;
+	setuid_jprobe.kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name("sys_setuid");
 
+	if(!setuid_jprobe.kp.addr)
+	{
+		printk("Unable to look up symbol\n");
+		return -1;
+	}
+
+	if((ret = register_jprobe(&setuid_jprobe))<0)
+	{
+		printk("register_jprobe failed, returned %d\n",ret);
+		return -1;
+	}
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	unregister_jprobe(&setuid_jprobe);
+	printk("jprobe unregistered\n");
+}
+module_init(jprobe_init)
+module_exit(jprobe_exit)
+MODULE_LICENSE("GPL");
